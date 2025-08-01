c2abfc7057062d8d45f221bf65941fe2931a34ec
yumiykim <ymkim030403@gmail.com>
2025-08-01 21:11:33 +0900

    Feat: Post 및 Comment 모델 생성

--- post/models.py
+++ post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    like_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.writer.username} - {self.content[:20]}"
